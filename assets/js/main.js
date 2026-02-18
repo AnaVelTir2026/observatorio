3f026e7425eeb5616e6c6b6460b90aa341932344
eclipse2026 <spainglutenfree@gmail.com>
2026-02-18 19:12:09 +0100

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-/* main.js - placeholder para funciones interactivas futuras
-   - Carrusel “Sabías que…”
-   - Lectura de RSS de MITECO
-   - Filtros/buscadores de publicaciones y proyectos
-*/
-console.log("Observatorio | JS cargado");
-``
